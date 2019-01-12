420fccd4b42b4b6982f527748bf414a32b8b00ac
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-12 23:36:51 +0900

    Profile Class 를 이용한 방법

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/TestBookRepository.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/TestBookRepository.java
@@ -3,5 +3,7 @@ package com.ubitoffee.inflearn.springapplicationcontext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+@Repository
+@Profile("test")
 public class TestBookRepository implements BookRepository {
 }
