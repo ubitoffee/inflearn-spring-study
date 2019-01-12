01649115c1c4778e84664938b8a7171453258604
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-12 20:34:09 +0900

    Prototype Bean 2 - Object Provider를 이용한 방법

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Proto.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Proto.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 // Thread-Safe 하지 않네
-@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component @Scope(value = "prototype")
 public class Proto {
 
     @Autowired
--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Single.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Single.java
@@ -1,15 +1,17 @@
 package com.ubitoffee.inflearn.springapplicationcontext;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Single {
 
+    // 스프링 코드가 들어가기에 비추
     @Autowired
-    Proto proto;
+    private ObjectProvider<Proto> proto;
 
     public Proto getProto() {
-        return proto;
+        return proto.getIfAvailable();
     }
 }
