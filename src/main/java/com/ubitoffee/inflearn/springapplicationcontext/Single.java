2a1f0ad618ec22b049a10bedc53b8049ffec157d
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-12 23:23:16 +0900

    Object Provider

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Single.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Single.java
@@ -1,17 +1,15 @@
 package com.ubitoffee.inflearn.springapplicationcontext;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Single {
 
-    // 스프링 코드가 들어가기에 비추
     @Autowired
-    private ObjectProvider<Proto> proto;
+    Proto proto;
 
     public Proto getProto() {
-        return proto.getIfAvailable();
+        return proto;
     }
 }
