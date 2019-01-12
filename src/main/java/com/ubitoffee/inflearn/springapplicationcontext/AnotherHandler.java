1071530614f62036cda3f0a05d716bcc3ab97ffb
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-13 00:36:46 +0900

    스프링 기본 이벤트들

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AnotherHandler.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AnotherHandler.java
@@ -1,14 +1,13 @@
 package com.ubitoffee.inflearn.springapplicationcontext;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
+    @Async
     public void handle(MyEvent myEvent) {
         System.out.println("Another handler: " + myEvent.getData());
     }
