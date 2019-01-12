7b51177db4895258bcf00279508f13fd413cb0f9
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-12 23:46:49 +0900

    Profile 설정방법 및 우선순위

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
@@ -1,6 +1,7 @@
 package com.ubitoffee.inflearn.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -18,10 +19,13 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     BookRepository bookRepository;
 
+    @Value("${app.name}")
+    String appName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name"));
+        System.out.println(appName);
     }
 }
