23b58b6799f8b6012fe700500298bbc7dd91d4e0
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-14 07:02:12 +0900

    PropertyEditorSupport를 활용한 방법

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/DemoApplication.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/DemoApplication.java
@@ -2,24 +2,12 @@ package com.ubitoffee.inflearn.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setCacheSeconds(3);
-        return messageSource;
     }
 
 }
--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Event.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/Event.java
@@ -2,8 +2,9 @@ package com.ubitoffee.inflearn.springapplicationcontext;
 
 public class Event {
 
-    Integer id;
-    String title;
+    private Integer id;
+
+    private String title;
 
     public Integer getId() {
         return id;
@@ -19,5 +20,17 @@ public class Event {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Event(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
