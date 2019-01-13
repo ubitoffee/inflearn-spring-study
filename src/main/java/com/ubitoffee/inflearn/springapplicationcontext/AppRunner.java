bb19fe926a574f355c607d01c8639d16f3e441e1
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-13 10:39:10 +0900

    ApplicationContext를 활용한 Resource 가져오기

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
@@ -3,18 +3,27 @@ package com.ubitoffee.inflearn.springapplicationcontext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher eventPublisher;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        eventPublisher.publishEvent(new MyEvent(this, 150));
-    }
+        System.out.println(resourceLoader.getClass());
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
 
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        // System.out.println(Files.readString(Path.of(resource.getURI()))); - 자바 11부터 제공
+    }
 }
