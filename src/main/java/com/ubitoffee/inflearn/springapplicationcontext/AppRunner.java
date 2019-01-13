e797f96a177d45a25ac17b67d1fcdf1e1973ebc1
SeongYeob Jeong <ubi.toffee@gmail.com>
2019-01-13 10:54:29 +0900

    예전의 Validator 사용방식

--- src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
+++ src/main/java/com/ubitoffee/inflearn/springapplicationcontext/AppRunner.java
@@ -1,29 +1,31 @@
 package com.ubitoffee.inflearn.springapplicationcontext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
-import java.nio.file.Files;
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
-        System.out.println(resource.getClass());
+        Event event = new Event();
+        EventValidator eventValidator = new EventValidator();
+        Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        // System.out.println(Files.readString(Path.of(resource.getURI()))); - 자바 11부터 제공
+        eventValidator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("========= error code =========");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
